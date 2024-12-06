e164d86cb8709af1c22f6941d6b3c384ef810718
grok <grok@home>
2024-12-06 13:50:07 +0300

    package rename

--- lib/views.dart
+++ lib/views.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-import 'package:darttonconnect/models/wallet_app.dart';
+import 'package:darttonconnect_plus/models/wallet_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/link.dart';
